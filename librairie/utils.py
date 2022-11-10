7d0762b6fdb52d61cc641f25a3ef7f00ee0efaae
Clément <clement.ramos-lage@seniormedia.fr>
2022-11-11 00:18:00 +0100

    feat: librairie eda outliers detections

---
+++ librairie/utils.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+def scatter_with_outliers(x, y, outliers_x=None, outliers_y=None, type="values"):
+  plt.scatter(x, y)
+  if type == "values":
+    plt.scatter(outliers_x, outliers_y, c="r")
+  elif type == "htresh":
+    plt.axhline(y=outliers_y, color="r")

607f2c44284a07f1528ab4157e255902895ae50d
Clement <clement.ramoslage@gmail.com>
2022-11-15 07:45:32 +0100

    feat: add some analysis

--- librairie/utils.py
+++ librairie/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 def scatter_with_outliers(x, y, outliers_x=None, outliers_y=None, type="values"):
@@ -6,3 +7,6 @@ def scatter_with_outliers(x, y, outliers_x=None, outliers_y=None, type="values")
     plt.scatter(outliers_x, outliers_y, c="r")
   elif type == "htresh":
     plt.axhline(y=outliers_y, color="r")
+
+def corr_mask(shape):
+  return np.triu(np.ones(shape)).astype(bool)
